0405d41b5d9d34feae9c63613ba7cb03c05e7237
Atharv Chowdhary <atharv_chowdhary@brown.edu>
2025-07-08 21:42:48 +0800

    added tfjs import, set up data loader

--- bigram.js
+++ bigram.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
+import * as tf from '@tensorflow/tfjs';
+import * as fs from 'fs';
 
 // hyperparameters
-BLOCK_SIZE = 8;
+const BATCH_SIZE = 4;
+const BLOCK_SIZE = 8;
 
 // read in data file
 const dataStr = fs.readFileSync('data.txt').toString();
-const splitInd = Math.round(0.9 * dataStr.length);
-const trainStr = dataStr.slice(0, splitInd);
-const valStr = dataStr.slice(splitInd);
 
 // set up token encoder and decoder
 const charList = Array.from(new Set(dataStr)).sort();
@@ -22,4 +21,32 @@ function decode(toks){
   return toks.map(i => itos.get(i)).join('');
 }
 
+// set up training and validation data
+const dataTensor = tf.tensor(encode(dataStr));
+const trainSize = Math.round(0.9 * dataTensor.size);
+const valSize = dataTensor.size - trainSize;
 
+const trainTensor = dataTensor.slice([0], [trainSize]);
+const valTensor = dataTensor.slice([trainSize], [valSize]);
+
+// set up data loader
+function getBatch(split){
+  // establish which data to use
+  let data = trainTensor;
+  if(split === "val"){
+    data = valTensor;
+  }
+  
+  // indices to sample from
+  let minInd = 0;
+  let maxInd = data.size - BLOCK_SIZE; 
+  const randInds = tf.randomUniform([BATCH_SIZE], minInd, maxInd, "int32").arraySync();
+
+  // get samples
+  for(let i = 0; i < BATCH_SIZE; i++){
+    let curSplit = randInds[i];
+    let curTensor = data.slice([curSplit], [BLOCK_SIZE]);
+    console.log(curTensor);
+  }
+}
+getBatch("train");
